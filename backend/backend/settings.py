9baabf74e21a4929ca02ff374ce255f208b8d1cf
coding-smit <smitp9328@gmail.com>
2026-02-20 13:08:49 +0530

    setting.py ALLOWED_HOST

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qg=wtd5it6nj$_%e^a02_effr1-!oc=6y!2qrr5q#$)s@37m-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['expernix.onrender.com']
 
 
 # Application definition
